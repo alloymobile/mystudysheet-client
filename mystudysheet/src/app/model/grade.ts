72850eb1d2482b6ed60179ab5cb85f6bb142d6fe
Tapas Mandal <tapas@alloymobile.ca>
2020-08-09 20:06:35 -0400

    YL00-1000#Create basuc template for mystudysheet

--- mystudysheet/src/app/model/grade.ts
+++ mystudysheet/src/app/model/grade.ts
@@ -1,13 +1,22 @@
+import { Subject } from './subject';
 export class Grade {
   id: number;
   name: string;
+  subjects: Subject[];
   constructor(grade?: any) {
     if (grade) {
-      this.id = Number(grade._embedded.metadata.id);
+      this.id = Number(grade.id);
       this.name = grade.name;
+      this.subjects = [];
+      if (grade.subjects && grade.subjects.length) {
+        grade.Subjects.array.forEach((element) => {
+          this.subjects.push(element);
+        });
+      }
     } else {
       this.id = 0;
-      this.name = "";
+      this.name = '';
+      this.subjects = [];
     }
   }
 }
--- mystudysheet/src/app/model/subject.ts
+++ mystudysheet/src/app/model/subject.ts
@@ -1,14 +1,22 @@
-import { Grade } from "./grade";
+import { Topic } from './topic';
 export class Subject {
   id: number;
   name: string;
+  topics; Topic[];
   constructor(subject?: any) {
     if (subject) {
-      this.id = Number(subject._embedded.metadata.id);
+      this.id = Number(subject.id);
       this.name = subject.name;
+      this.topics = [];
+      if(subject.topics && subject.topics.length > 0){
+        subject.topics.forEach(element => {
+            return this.topics.push(new Topic(element));
+         });
+      }
     } else {
       this.id = 0;
-      this.name = "";
+      this.name = '';
+      this.topics = [];
     }
   }
 }
